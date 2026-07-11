ac41e0683d263db096d8f5ebe1812b6fd222db11
yash-2008-yash <yashwanthm897@gmail.com>
2026-07-11 22:21:20 +0530

    Initial commit
    Completed the whole UI part. Just have to add functionality.

--- app/page.js
+++ app/page.js
@@ -1,65 +1,80 @@
 import Image from "next/image";
+import Separator from "@/components/Separator";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  return (<>
+
+    {/* Container */}
+    <div className="bg-black max-w-200 rounded-2xl mx-auto flex flex-col text-center">
+
+      {/* Heading */}
+      <h1 className="text-3xl stack-sans-font bg-white text-black rounded-t-2xl">MY LINKTREE</h1>
+
+      {/* Hero Section */}
+      <div className="mx-10 mt-10 mb-20">
+        <Image src="/profile.jpg" alt="Profile Picture" width={150} height={150} className="rounded-full mx-auto w-[150px] h-[150px]" />
+
+        <p className="text-xl stack-sans-font mt-5">Hello! I&apos;m...</p>
+        <h1 className="text-3xl sm:text-6xl notable-font font-bold">Yashwanth M</h1>
+      </div>
+
+      <Separator />
+
+      {/* Socials */}
+      <div className="my-5">
+        <h1 className="text-xl stack-sans-font">SOCIALS</h1>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 min-h-10 rounded-full flex justify-center items-center">
+          <Image src="/github.jpg" alt="GitHub" width={80} height={80} />
+          <h1 className="text-xl stack-sans-font">GitHub</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 mb-10 min-h-20 rounded-full flex justify-center items-center gap-2">
+          <Image src="/linkedin.jpg" alt="LinkedIn" width={65} height={65} />
+          <h1 className="text-xl stack-sans-font">LinkedIn</h1>
         </div>
-      </main>
+      </div>
+
+      <Separator />
+
+      {/* Projects */}
+      <div className="my-5">
+        <h1 className="text-xl stack-sans-font">PROJECTS</h1>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 min-h-20 rounded-full flex justify-center items-center gap-3">
+          <Image src="/shawty.jpg" alt="GitHub" width={40} height={40} className="rounded-lg" />
+          <h1 className="text-xl stack-sans-font">ShawtY</h1>
+        </div>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 min-h-20 rounded-full flex justify-center items-center gap-3">
+          <Image src="/yashlist.jpg" alt="GitHub" width={50} height={50} className="rounded-lg" />
+          <h1 className="text-xl stack-sans-font">yashlist.</h1>
+        </div>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 mb-10 min-h-20 rounded-full flex justify-center items-center gap-3">
+          <Image src="/memezilla.jpg" alt="GitHub" width={50} height={50} className="rounded-lg" />
+          <h1 className="text-xl stack-sans-font">MemeZilla</h1>
+        </div>
+      </div>
+
+      <Separator />
+
+      {/* Contact */}
+      <div className="my-5">
+        <h1 className="text-xl stack-sans-font">CONTACT</h1>
+
+        <div className="bg-[rgb(10,10,10)] mx-10 mt-3 min-h-20 rounded-full flex justify-center items-center gap-3">
+          <Image src="/gmail.jpg" alt="GitHub" width={50} height={50} className="rounded-lg" />
+          <h1 className="text-xl stack-sans-font">Gmail</h1>
+        </div>
+      </div>
+
+      {/* Footer */}
+      <div className="mb-10 mt-5 flex justify-center gap-1.5 stack-sans-font">
+        <h1 className="text-xl">Made by</h1>
+        <h1 className="text-xl font-bold">YASHWANTH M</h1>
+      </div>
+
     </div>
-  );
+  </>);
 }
---
+++ components/Separator.js
@@ -0,0 +1,6 @@
+export default function Separator() {
+  
+  return (<>
+    <div className="h-px w-50 bg-white mx-auto"></div>
+  </>)
+}
